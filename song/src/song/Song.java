334f484f3dbeda114dfb069f277ae5600eb89e36
Robert Romero <21332@columbushs.com>
2019-09-17 10:40:18 -0400

    Crocodile Rock

--- song/src/song/Song.java
+++ song/src/song/Song.java
@@ -15,9 +15,54 @@ public class Song {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Code inside the main method
+        System.out.println("I remember when rock was young");
+        System.out.println("Me and Suzie had so much fun");
+        System.out.println("Holding hands and skimming stones");
+        System.out.println("Had an old gold Chevy and a place of my own");
+        System.out.println("But the biggest kick I ever got");
+        System.out.println("Was doing a thing called the Crocodile Rock");
+        System.out.println("While the other kids were Rocking Round the Clock");
+        System.out.println("We were hopping and bopping to the Crocodile Rock");
         
+        chorus();
+        
+        System.out.println("But the years went by and the rock just died");
+        System.out.println("Suzie went and left us for some foreign guy");
+        System.out.println("Long nights crying by the record machine");
+        System.out.println("Dreaming of my Chevy and my old blue jeans");
+        System.out.println("But they'll never kill the thrills we've got");
+        System.out.println("Burning up to the Crocodile Rock");
+        System.out.println("Learning fast as the weeks went past");
+        System.out.println("We really thought the Crocodile Rock would last");
+        System.out.println("");
+        
+        chorus();
+        
+        System.out.println("I remember when rock was young");
+        System.out.println("Me and Suzie had so much fun");
+        System.out.println("Holding hands and skimming stones");
+        System.out.println("Had an old gold Chevy and a place of my own");
+        System.out.println("But the biggest kick I ever got");
+        System.out.println("Was doing a thing called the Crocodile Rock");
+        System.out.println("While the other kids were Rocking Round the Clock");
+        System.out.println("We were hopping and bopping to the Crocodile Rock");
+   
+        
+        chorus();
+        System.out.println("La lalalala la lalalala la lalalala la");
+        System.out.println("La lalalala la lalalala la lalalala la");
             
+    }
+    public static void chorus(){
+        System.out.println("Well Crocodile Rocking is something shocking");
+        System.out.println("When your feet just can't keep still");
+        System.out.println("I never knew me a better time and I guess I never will");
+        System.out.println("Oh Lawdy mama those Friday nights");
+        System.out.println("When Suzie wore her dresses tight");
+        System.out.println("And the Crocodile Rocking was out of sight");
+        System.out.println("");
+        System.out.println("La lalalala la lalalala la lalalala la");
+        System.out.println("");
     }
     
 }
